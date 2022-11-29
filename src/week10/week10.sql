9e41b68e1ea3287f5e1668f410f983314bc0bbfe
Dimitar <dim.damianov@gmail.com>
2022-11-29 14:11:34 +0200

    m

---
+++ src/week10/week10.sql
@@ -0,0 +1,8 @@
+SELECT AVG(speed) FROM pc;
+SELECT product.maker, AVG(laptop.screen) FROM product, laptop WHERE product.model = laptop.model GROUP BY product.maker;
+SELECT AVG(laptop.speed) FROM laptop WHERE laptop.price > 1000;
+SELECT product.maker, AVG(pc.price) FROM product, pc WHERE product.model = pc.model GROUP BY product.maker HAVING product.maker = 'A';
+SELECT speed, AVG(PRICE) FROM pc GROUP BY speed;
+
+SELECT product.maker, MAX(pc.price) FROM product, pc WHERE pc.model = product.model;
+SELECT speed, AVG(PRICE) FROM pc WHERE speed > 800 GROUP BY speed;
